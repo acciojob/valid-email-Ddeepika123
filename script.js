5c4bd5c36676532d6869667d020870528c6711fa
Ddeepika123 <112195696+Ddeepika123@users.noreply.github.com>
2026-08-15 23:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function validEmail(str) {
   //your JS code here.
+	const regex = /^[A-Za-z0-9]+(?:[._-][A-Za-z0-9]+)*@[A-Za-z0-9]+(?:[.-][A-Za-z0-9]+)*\.[A-Za-z]{2,3}$/;
+    return regex.test(str);
 }
-
+console.log(validEmail("abc@example.com"));   
+console.log(validEmail("xyz@abc.com.in"));     
+console.log(validEmail("john.doe@com."));     
+console.log(validEmail("@example.com"));       
+console.log(validEmail(""));   
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
